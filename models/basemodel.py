a92349e9edf9fec91719244123d5bcbba6172ce7
adryells <adryellpaulo@gmail.com>
2021-11-20 15:55:34 -0300

    -Remoção de campos e tabelas não mais necessárias

--- models/basemodel.py
+++ models/basemodel.py
@@ -1,14 +1,12 @@
 from sqlalchemy.orm import Session, declarative_base
 from sqlalchemy import create_engine, Column, Integer, String, Boolean
-# from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 class Config:
     Base = declarative_base()
-    engine = create_engine("sqlite:///../YELLpersonalpage.db", echo=True, future=True,
+    engine = create_engine("sqlite:///YELLpersonalpage.db", echo=True, future=True,
                            connect_args={'check_same_thread': False})
     session = Session(engine, future=True)
-    #session = scoped_session(sessionmaker(bind=engine))
 
 
 class Post(Config.Base):
@@ -18,7 +16,6 @@ class Post(Config.Base):
     title = Column(String, nullable=False)
     description = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=False)
     media = Column(String)
     active = Column(Boolean, default=True)
 
@@ -35,7 +32,6 @@ class Project(Config.Base):
     bigdescription = Column(String, default=shortdescription)
     link = Column(String, nullable=False)
     media = Column(String)
-    published = Column(Boolean)
     active = Column(Boolean, default=True)
 
     def __repr__(self):
@@ -55,18 +51,3 @@ class Social(Config.Base):
     def __repr__(self):
         return f"Social(id={self.socialid}, name={self.name}"
 
-
-class Technology(Config.Base):
-    __tablename__ = 'technologies'
-
-    technologyid = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    description = Column(String, nullable=False)
-    link = Column(String, nullable=False)
-    experience = Column(String, nullable=False)
-    media = Column(String)
-    situation = Column(String)
-    active = Column(Boolean, default=True)
-
-    def __repr__(self):
-        return f"Technology(id={self.technologyid}, name={self.name}"
