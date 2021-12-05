a154145d5a32864bfb562b526902925fa43477b9
adryells <adryellpaulo@gmail.com>
2021-12-05 01:33:10 -0300

    - Adição de tags

---
+++ controllers/tagcontroller.py
@@ -0,0 +1,68 @@
+
+from random import choice
+
+from sqlalchemy import select
+
+from models.basemodel import Config, tag_posts, tag_projects, Tag, Post, Project
+from sqlalchemy.inspection import inspect
+
+
+class TagController:
+
+    def add_tag(self, name: str) -> Tag:
+        session = Config.session
+
+        tag = Tag(name=name)
+
+        session.add(tag)
+        session.commit()
+
+
+    def update_tag(self, tagid: int, name: str):
+        session = Config.session
+        tag = session.query(Tag).filter_by(tagid=tagid).one()
+
+        if not tag:
+            raise Exception("Tag not found")
+
+        tag.name = name
+
+        session.commit()
+
+    def get_tags(self) -> list:
+        session = Config.session
+
+        projects = session.query(Tag).filter(Tag.active == True).all()
+
+        return projects
+
+    def get_tag_by_id(self, tagid: int):
+        session = Config.session
+        tag = session.query(Tag).filter(Tag.tagid==tagid).one()
+
+        if not tag:
+            raise Exception("Tag not found")
+
+        return tag
+
+    def get_tags_from_post(self, postid: int) -> list:
+        session = Config.session
+
+        object = session.query(tag_posts).filter(tag_posts.constraints == postid).all()
+
+        return object
+
+    def get_tags_from_project(self, projectid: int) -> list:
+        session = Config.session
+
+        object = session.query(tag_projects).join(Project).filter(tag_projects.c.project_id == projectid).all()
+
+        return object
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 
 from controllers.postcontroller import PostController
 from controllers.projectcontroller import ProjectController
 from controllers.socialcontroller import  SocialController
+from controllers.tagcontroller import TagController
+from models.basemodel import Tag, Config
 
 app = Flask(__name__)
 
@@ -13,7 +15,7 @@ def index():
     social_controller = SocialController
     project_controller = ProjectController
 
-    social =  social_controller.get_social_medias(social_controller)
+    social = social_controller.get_social_medias(social_controller)
     project = project_controller.get_projects_to_home(project_controller)
 
     return render_template('index.html', socials=social, projects=project)
@@ -21,7 +23,11 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     social_controller = SocialController
+    tagcontroller = TagController
+
+    print(tagcontroller.get_tags_from_project(tagcontroller, 1))
     social = social_controller.get_social_medias(social_controller)
+
     return render_template('404.html', socials=social), 404
 
 
@@ -30,16 +36,45 @@ def page_not_found_500(e):
     social_controller = SocialController
     social = social_controller.get_social_medias(social_controller)
     return render_template('404.html', socials=social), 500
+
 @app.route('/blog', methods=['GET'])
-def blog():
+@app.route('/blog/<orderby>', methods=['GET'])
+def blog(orderby:str="recent"):
 
     blog_controller = PostController
     social_controller = SocialController
 
-    blog = blog_controller.get_posts(blog_controller)
+    blog = blog_controller.get_posts(blog_controller, orderby)
     social = social_controller.get_social_medias(social_controller)
 
     return render_template('blog.html', posts=blog, socials=social)
+
+@app.route('/blog/add', methods=['GET', 'POST'])
+def add_post():
+
+    blog_controller = PostController
+
+    post = blog_controller.add_post(blog_controller,
+                                    title="ue",
+                                    active=True,
+                                    content="kkkkk",
+                                    description="kkkk",
+                                    media="media",
+                                    tags=[1,2,3]
+                                    )
+    return "Post adicionado"
+
+@app.route('/blog/update/<int:postid>', methods=['GET', 'PUT'])
+def update_post(postid:int):
+    session = Config.session
+
+    blog_controller = PostController
+
+    tags = [session.query(Tag).filter(Tag.tagid == tagid).one() for tagid in [5,6,4]]
+    blog_controller.update_post(blog_controller,postid,{"title":"mentira sio", "active":False, "content":"jjjj", "description":"llll", "media":"kkskksks", "tags":tags})
+
+    return f"Post {postid} atualizado"
+
 
 
 @app.route('/blog/<int:postid>', methods=['GET'])
@@ -57,21 +92,21 @@ def blog_post(postid: int = 1):
     if not post:
         return render_template('404.html'), 404
 
-
     return render_template('post.html', post=post, posts=posts, socials=social, projects=projects)
 
 
-
 @app.route('/projects', methods=['GET'])
-def projects():
+@app.route('/projects/<orderby>', methods=['GET'])
+def projects(orderby:str = "recent"):
 
     project_controller = ProjectController
     social_controller = SocialController
 
-    projects = project_controller.get_projects(project_controller)
+    projects = project_controller.get_projects(project_controller, orderby)
     social = social_controller.get_social_medias(social_controller)
 
     return render_template('projects.html', projects=projects, socials=social)
+
 
 @app.route('/paymeacoffee', methods=['GET'])
 def pay_me_a_coffee():
@@ -79,6 +114,7 @@ def pay_me_a_coffee():
     social = social_controller.get_social_medias(social_controller)
 
     return render_template('paymeacoffee.html', socials=social)
+
 
 @app.route('/administracao_do_portifolio', methods=['GET'])
 def admin():
